5d26b5e3a4742c1031841e1f4291dc16c0d4fa95
Tsypin Ilya <ilyaboy@list.ru>
2022-08-15 13:52:54 +0300

    Solution for ElephantOutOfFly

---
+++ ElephantOutOfFly/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ElephantOutOfFly;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        TransformToElephant();
+        Console.WriteLine("Муха");
+        //... custom application code
+    }
+    
+    private static void TransformToElephant()
+    {
+        Console.SetOut(new TransformToElephantTextWriter(Console.Out));
+    }
+
+    private class TransformToElephantTextWriter : StringWriter
+    {
+        private readonly TextWriter _out;
+
+        public TransformToElephantTextWriter(TextWriter @out)
+        {
+            _out = @out;
+        }
+
+        public override void WriteLine(string? value)
+        {
+            if (value == "Муха")
+            {
+                value = "Слон";
+            }
+         
+            _out.WriteLine(value);
+        }
+    }
+}
