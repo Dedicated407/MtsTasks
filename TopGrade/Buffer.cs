4aa5432224beadf2b0f7bc967da7cb403b32ca67
Tsypin Ilya <ilyaboy@list.ru>
2022-08-16 18:16:48 +0300

    Solution for TopGrade

---
+++ TopGrade/Buffer.cs
@@ -0,0 +1,60 @@
+﻿namespace TopGrade;
+
+public class Buffer
+{
+    private readonly int[] _indexes;
+    private readonly int _sortFactor;
+    private int _previousLeftBorder = 0;
+    private int _leftBorder = 0;
+        
+    public Buffer(int sortFactor, int maxValue)
+    {
+        _indexes = new int[maxValue + 1];
+        _sortFactor = sortFactor;
+    }
+
+    public void AddValue(int value)
+    {
+        var border = value - (_sortFactor + 1);
+            
+        if (border > 0)
+        { 
+            MoveBorder(border);
+        }
+            
+        this[value]++;
+    }
+
+    private void MoveBorder(int border)
+    {
+        _previousLeftBorder = _leftBorder;
+        _leftBorder = border;
+    }
+
+    public void FlushBuffer()
+    {
+        MoveBorder(_indexes.Length);
+    }
+
+    public IEnumerable<int> GetCurrentSequence()
+    {
+        for (int i = _previousLeftBorder; i < _leftBorder; i++)
+        {
+            for (int j = 0; j < this[i]; j++)
+            {
+                yield return i;   
+            }
+        }
+    }
+
+    private int this[int i]
+    {
+        get => _indexes[GetIndex(i)];
+        set => _indexes[GetIndex(i)] = value;
+    }
+
+    private int GetIndex(int index)
+    {
+        return index;
+    }
+}
---
+++ TopGrade/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TopGrade;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var result = Sort(new[] {1, 4, 2, 4, 5, 7}, 2, 7);
+        foreach (var i in result)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+
+    /// <summary>
+    /// Возвращает отсортированный по возрастанию поток чисел
+    /// </summary>
+    /// <param name="inputStream">Поток чисел от 0 до maxValue. Длина потока не превышает миллиарда чисел.</param>
+    /// <param name="sortFactor">Фактор упорядоченности потока. Неотрицательное число. Если в потоке встретилось число x, то в нём больше не встретятся числа меньше, чем (x - sortFactor).</param>
+    /// <param name="maxValue">Максимально возможное значение чисел в потоке. Неотрицательное число, не превышающее 2000.</param>
+    /// <returns>Отсортированный по возрастанию поток чисел.</returns>
+    private static IEnumerable<int> Sort(IEnumerable<int> inputStream, int sortFactor, int maxValue)
+    {
+        var buffer = new Buffer(sortFactor, maxValue);
+
+        foreach (int value in inputStream)
+        {
+            buffer.AddValue(value);
+            
+            foreach (var i in buffer.GetCurrentSequence())
+            {
+                yield return i;
+            }
+        }
+
+        buffer.FlushBuffer();
+
+        foreach (int i in buffer.GetCurrentSequence())
+        {
+            yield return i;
+        }
+    }
+}
